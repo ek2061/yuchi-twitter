f2538460125620686d6259a7c4ce08c21fcec863
ek2061 <jackey860517@gmail.com>
2023-01-01 17:06:40 +0800

    fix: fix tailwindcss does not working

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
+const tailwindConfig = require("./tailwind.config");
+
 module.exports = {
   plugins: {
-    // tailwindcss: {},
+    tailwindcss: { tailwindConfig },
     autoprefixer: {},
   },
 };
