5515608d7dfc7185cdfddefd650c603659e04a2d
ek2061 <jackey860517@gmail.com>
2023-01-02 13:50:12 +0800

    fix: add missing Toaster

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { GetServerSideProps } from "next";
 import Head from "next/head";
+import { Toaster } from "react-hot-toast";
 import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 import Widgets from "../components/Widgets";
@@ -16,6 +17,7 @@ const Home = ({ tweets }: HomeProps) => {
       <Head>
         <title>Yuchi Twitter</title>
       </Head>
+      <Toaster />
       <main className="grid grid-cols-9">
         <Sidebar />
         <Feed tweets={tweets} />
